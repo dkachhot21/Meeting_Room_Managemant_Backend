62cf255f589816bf7d17f4938b88d93043204832
dkachhot21 <dkachhot21@gmail.com>
2024-04-08 22:06:29 +0530

    Render Deploment Server Added

--- swagger.js
+++ swagger.js
@@ -14,6 +14,10 @@ const options = {
                 url: "http://localhost:3000",
                 description: "Development server",
             },
+            {
+                url: "https://meeting-room-managemant-backend.onrender.com",
+                description: "Deployment Server",
+            },
         ],
         components: {
             schemas: {
